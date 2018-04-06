a19d4206b582072de0f61a85839deb44920b5bd3
Kirill Varlamov <k.v.varlamov@gmail.com>
2018-04-06 19:11:13 +0300

    LTY-74 Move price oracle functionality to the external Oracle contract (#15)
    Remove python tests

--- migrations/2_lty_token.js
+++ migrations/2_lty_token.js
@@ -1,13 +1,15 @@
 var LongevityToken = artifacts.require("LongevityToken");
 var LongevityCrowdsale = artifacts.require("LongevityCrowdsale");
+var PriceOracleMock = artifacts.require("PriceOracleMock");
 
 module.exports = function (deployer) {
     // deployment steps
+    deployer.deploy(PriceOracleMock);
     deployer.deploy(LongevityToken);
     LongevityToken.deployed().then(function (nToken) {
         console.log("##########" + nToken.address);
         nToken.setMintTap(99999999999999999999);
-        deployer.deploy(LongevityCrowdsale, nToken.address, 130671).then(function () {
+        deployer.deploy(LongevityCrowdsale, nToken.address, PriceOracleMock.address).then(function () {
             LongevityCrowdsale.deployed().then(function (nCS) {
                 console.log("##########" + nCS.address);
                 nToken.addMinter(nCS.address);
